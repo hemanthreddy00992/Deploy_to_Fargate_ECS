58e1c7aade01fd5949c833c16e6ae3e075b77eb4
Hemanth reddy <130210051+hemanthreddy00992@users.noreply.github.com>
2024-12-16 12:10:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def greet(temp):
     return f'{temp}--> This is flask application deployed on Fargate in ECS'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
